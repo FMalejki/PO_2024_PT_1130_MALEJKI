9065d287171679ec8e2793953fb6c7da3574ecc1
FMalejki <fmalejkI@proton.me>
2024-10-30 15:18:44 +0100

    checks if MapDirection and Vector2d are working properly

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -1,5 +1,6 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MapDirection;
 import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
 
@@ -9,11 +10,23 @@ public class World {
         MoveDirection[] directions = OptionParser.refactor(args);
         run(directions);
         System.out.println("STOP");
+//        check if Vector2d works as it should
         Vector2d position1 = new Vector2d(1,2);
         System.out.println(position1);
         Vector2d position2 = new Vector2d(-2,1);
         System.out.println(position2);
         System.out.println(position1.add(position2));
+
+//        check if MapDirection works as it should
+        MapDirection direction = MapDirection.NORTH;
+        System.out.println(direction);
+        direction = direction.next();
+        System.out.println(direction);
+        direction = direction.previous();
+        direction = direction.previous();
+        System.out.println(direction);
+        Vector2d dirVec = direction.toUnitVector();
+        System.out.println(dirVec);
     }
 
     public static void run(MoveDirection[] directions) {
