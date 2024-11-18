13fee6a998b51182d6b8d874a355b5948a715c38
FMalejki <fmalejkI@proton.me>
2024-11-18 23:04:50 +0100

    commit before merging lab4

--- src/main/java/agh/ics/oop/model/util/GrassField.java
+++ src/main/java/agh/ics/oop/model/util/GrassField.java
@@ -5,12 +5,31 @@ import agh.ics.oop.model.MoveDirection;
 import agh.ics.oop.model.Vector2d;
 import agh.ics.oop.model.WorldMap;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class GrassField implements WorldMap {
 
     private int number;
 
     public GrassField(int number) {
         this.number = number;
+    }
+
+    private Vector2d rng(Vector2d rangeMax) {
+        Vector2d rangeMin = new Vector2d(0,0);
+        //rangeMax
+
+        int i = 0;
+        while(i < this.number) {
+            int randomNumX = ThreadLocalRandom.current().nextInt(rangeMin.getX(), rangeMax.getX() + 1);
+            int randomNumY = ThreadLocalRandom.current().nextInt(rangeMin.getY(), rangeMax.getY() + 1);
+
+        }
+        System.out.println("Random number: " + randomNum);
+
     }
 
     @Override
