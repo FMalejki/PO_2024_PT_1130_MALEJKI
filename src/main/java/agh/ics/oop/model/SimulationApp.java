f989a0f256d05ac4b3f20f8bf14ab981aeba96c9
FMalejki <fmalejkI@proton.me>
2024-12-18 17:18:42 +0100

    javafx skeleton

---
+++ src/main/java/agh/ics/oop/model/SimulationApp.java
@@ -0,0 +1,9 @@
+package agh.ics.oop.model;
+
+import javafx.stage.Stage;
+
+public class SimulationApp extends javafx.application.Application {
+    public void start(Stage primaryStage){
+        primaryStage.show();
+    }
+}
